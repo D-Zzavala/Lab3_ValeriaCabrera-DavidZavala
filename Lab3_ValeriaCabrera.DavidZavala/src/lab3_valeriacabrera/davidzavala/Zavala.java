845632e3338a0cbed7e38285bfd138ca5aecc336
D-Zzavala <65042896+D-Zzavala@users.noreply.github.com>
2020-08-07 18:58:55 -0600

    no funcionaba en el otro repo

--- Lab3_ValeriaCabrera.DavidZavala/src/lab3_valeriacabrera/davidzavala/Zavala.java
+++ Lab3_ValeriaCabrera.DavidZavala/src/lab3_valeriacabrera/davidzavala/Zavala.java
@@ -5,55 +5,146 @@
  */
 package lab3_valeriacabrera.davidzavala;
 
-import static lab3_valeriacabrera.davidzavala.Lab3_ValeriaCabreraDavidZavala.read;
 
 public class Zavala {
 
-    char sn = ' ';
-    boolean login = false;
-    boolean clau = false;
-    String sudo = "SUDO";
-    String sudopw = "clau123";
-    while (sn
-
+    /*
     
-        == ' ') {
-            System.out.println("1. Log in");
-        int op = read.nextInt();
-        switch (op) {
-            case 1: {
-                //Validacion login hecho ya
-                if (login == true) {
-                    System.out.println("El login ya se realizo.");
-                } else if (login == false) {
-                    //realiza login
-                    System.out.println("Usuario: ");
-                    String usuario = read.next();
-                    System.out.println("Contrasena: ");
-                    String contrasena = read.next();
-                    //validaciones login
-                    if (usuario.equals(sudo) && contrasena.equals(sudopw)) {
-                        clau = true;
-                        login = true;
+    
+    if (clau == true && login == true) {
+                        char ssnn = 'l';
+                        while (ssnn == 'l') {
+                            int op2 = read.nextInt();
+                            switch (op2) {
+                                case 1: {
+                                    //Esto es de Vale
+                                    break;
+                                }
+                                case 2: {
+                                    //mod
+                                    System.out.println("1. persona \n2. Pisos \n0. Salir");
+                                    int sel = read.nextInt();
+                                    switch (sel) {
+                                        case 1: {
+
+                                            break;
+                                        }
+                                        case 2: {
+
+                                            break;
+                                        }
+                                        case 0: {
+
+                                            break;
+                                        }
+                                        default: {
+                                            System.out.println("Error");
+                                            break;
+                                        }
+                                    }
+                                    break;
+                                }
+                                case 3: {
+                                    //remove
+                                    System.out.println("1. persona \n2. Pisos 0. \nsalir");
+                                    int sel = read.nextInt();
+                                    switch (sel) {
+                                        case 1: {
+                                            break;
+                                        }
+                                        case 2: {
+                                            break;
+                                        }
+                                        case 0: {
+                                            break;
+                                        }
+                                        default: {
+                                            break;
+                                        }
+                                    }
+                                    break;
+                                }
+                                case 0: {
+                                    //salir
+                                    sn = 's';
+                                    break;
+                                }
+                                default: {
+                                    break;
+                                }
+                            }
+                        }
                     }
-                    if (clau) {                                                        //usuarios normales
-                                 
-                    } else if (clau) {
+    
+    
+    */
+    
+    /* 
+     static Scanner read = new Scanner(System.in);
+    static EstadodeRegistro EsRe = new EstadodeRegistro();
+    static Inferior Infe = new Inferior();
+    static Irregular Irre = new Irregular();
+    static Normal Norm = new Normal();
+    static Personas Pers = new Personas();
+    static Superior Super = new Superior();
+    static Regular Regu = new Regular();
+    static Ranker Rank = new Ranker();
+    static Pruebas Prue = new Pruebas();
+    static ArrayList<Personas> APersonas = new ArrayList();
+    static ArrayList<String> ANombres = new ArrayList();
+
+    */
+    
+     /*
+    char sn = ' ';
+        boolean login = false;
+        boolean clau = false;
+
+        String sudo = "SUDO";
+        String sudopw = "clau123";
+
+        while (sn == ' ') {
+            System.out.println("1. Log in \n2. Sign in \n0. Salir");
+            int op = read.nextInt();
+            switch (op) {
+                case 1: { // Log
+                    boolean lock = false;
+                    while (lock == false) {
+                        System.out.println("Usuario: ");
+                        String user = read.next();
+                        System.out.println("Password");
+                        String password = read.next();
+
+                        //Superusuario
+                        if (user.equals(sudo) && password.equals(sudopw)) {
+                            clau = true;
+                        }
+
+                        //usuariosgatos
+                        if (ANombres.contains(user)) {
+                            login = true;
+                        } else {
+                            System.out.println("Usuario no registrado");
+                        }
 
                     }
-
+                    break;
                 }
-                break;
+                case 2: { //sign
+
+                    break;
+                }
+                case 0: {
+                    sn = 'g';
+                    break;
+                }
+                default:
+                    System.out.println("Error");
+                    break;
             }
-            case 3: {
-                break;
-            }
-            case 0: {
-                break;
-            }
-            default: {
-                break;
-            }
+
         }
+          */
+    
     }
-}
+
